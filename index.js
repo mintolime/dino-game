413cf002aac4ec50e65722d45598604c32f96ce0
mintolime <104442896+mintolime@users.noreply.github.com>
2023-04-29 02:21:28 +0000

    dev: constants moved to a separate directory

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const pageGame = document.querySelector('.game__box');
-const pageStart = document.querySelector('.page-start');
-const pageEnd = document.querySelector('.page-end');
-const pageGameOver = document.querySelector('.page-final');
-const dino = document.querySelector('#dino');
-const cactus = document.querySelector('#cactus');
-const exitBtn = document.querySelector('.game__btn-quit');
-const backBtn = document.querySelector('.game__btn-back');
-const jumpBtn = document.querySelector('.game__btn-jump');
-const yesBtn = pageStart.querySelector('.game__btn-yes');
-const noneBtn = pageStart.querySelector('.game__btn-no');
-const playAgainBtn = pageGameOver.querySelector('.game__btn-return');
-const gameScore = document.querySelector('.game__score span');
-const gameScoreFinal = document.querySelector('.game__score_final span');
-
+import {
+  pageGame,
+  pageStart,
+  pageEnd ,
+  pageGameOver,
+  dino ,
+  cactus,
+  exitBtn ,
+  backBtn ,
+  jumpBtn,
+  yesBtn,
+  noneBtn,
+  playAgainBtn,
+  gameScore,
+  gameScoreFinal
+} from './utils/constants.js'
 let score = 0;
-
+console.log(pageGame)
 const fix = (evt) => {
   console.log(evt.target);
 };
@@ -49,7 +50,7 @@ const jump = () => {
   }
   setTimeout(function () {
     if (cactus.offsetLeft < 0) {
-      score += 20;
+      score += 10;
       gameScore.textContent = score;
     }
     dino.classList.remove('jump');
---
+++ utils/constants.js
@@ -0,0 +1,15 @@
+const pageGame = document.querySelector('.game__box');
+const pageStart = document.querySelector('.page-start');
+const pageEnd = document.querySelector('.page-end');
+const pageGameOver = document.querySelector('.page-final');
+const dino = document.querySelector('#dino');
+const cactus = document.querySelector('#cactus');
+const exitBtn = document.querySelector('.game__btn-quit');
+const backBtn = document.querySelector('.game__btn-back');
+const jumpBtn = document.querySelector('.game__btn-jump');
+const yesBtn = pageStart.querySelector('.game__btn-yes');
+const noneBtn = pageStart.querySelector('.game__btn-no');
+const playAgainBtn = pageGameOver.querySelector('.game__btn-return');
+const gameScore = document.querySelector('.game__score span');
+const gameScoreFinal = document.querySelector('.game__score_final span');
+
